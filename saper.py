e5cd6bdee8e1db31d2fd55fc1dd2c7e12072519a
YaRo <roman.yatsenko@hneu.net>
2026-04-04 16:05:37 +0300

    Add game consts and Cell class

--- saper.py
+++ saper.py
@@ -1,9 +1,43 @@
 import flet as ft
+import random
+import time
+
+# Рівні складності: (розмір поля, кількість мін)
+LEVELS = (
+    (8, 10),
+    (16, 40),
+    {24, 99},
+)
+
+# Статуси гри
+STATUS_READY = 0
+STATUS_PLAY = 1
+STATUS_FAILED = 2
+STATUS_SUCCESS = 3
+
+CELL_SIZE = 30
+
+
+class Cell:
+    """Стан однієї клітинки ігрового поля"""
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+        self.reset()
+
+    def reset(self):
+        self.is_mine = False
+        self.is_revealed = False
+        self.is_flagged = False
+        self.is_start = False
+        self.is_end = False
+        self.mines_around = 0
 
 
 def main(page: ft.Page):
     page.title = "Сапер"
-    page.add(ft.Text("Привіт, Сапер!", size=30))
+    page.add(ft.Text("Сапер (Крок 2)", size=30))
 
 
 ft.run(main)
