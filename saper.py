e081db67670a2e7eca16d4b4ab6c5a5611154209
YaRo <roman.yatsenko@hneu.net>
2026-04-04 16:30:40 +0300

    Implement MineSweeper class and UI grid setup

--- saper.py
+++ saper.py
@@ -35,9 +35,67 @@ class Cell:
         self.mines_around = 0
 
 
+class MineSweeper:
+    """Головний клас застосунку Сапер"""
+
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.page.title = "Сапер"
+        self.page.padding = 10
+        self.page.scroll = ft.ScrollMode.AUTO
+
+        self.level = 0
+        self.board_size, self.mines_count = LEVELS[self.level]
+
+        self.cells: list[list[Cell]] = []
+        self.cell_containers: list[list[ft.Container]] = []
+
+        self._build_ui()
+        self._build_grid()
+        self.page.update()
+
+    def _build_ui(self):
+        """Створення інтерфейсу"""
+        self.grid_column = ft.Column(
+            spacing=1,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        )
+        self.page.add(self.grid_column)
+
+    def _build_grid(self):
+        """Побудова ігрового поля"""
+        self.cells = []
+        self.cell_containers = []
+        self.grid_column.controls.clear()
+
+        for x in range(self.board_size):
+            row_cells = []
+            row_containers = []
+            row = ft.Row(spacing=1, alignment=ft.MainAxisAlignment.CENTER)
+
+            for y in range(self.board_size):
+                cell = Cell(x, y)
+                row_cells.append(cell)
+
+                container = ft.Container(
+                    width=CELL_SIZE,
+                    height=CELL_SIZE,
+                    bgcolor=ft.Colors.BLUE_GREY_300,
+                    border_radius=2,
+                    border=ft.Border.all(1, ft.Colors.GREY_500),
+                    alignment=ft.Alignment.CENTER,
+                )
+
+                row_containers.append(container)
+                row.controls.append(container)
+
+            self.cells.append(row_cells)
+            self.cell_containers.append(row_containers)
+            self.grid_column.controls.append(row)
+
+
 def main(page: ft.Page):
-    page.title = "Сапер"
-    page.add(ft.Text("Сапер (Крок 2)", size=30))
+    MineSweeper(page)
 
 
 ft.run(main)
